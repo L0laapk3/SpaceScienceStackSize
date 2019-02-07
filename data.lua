920183ea937c2d7932edebaa3005f81b45d81623
L0laapk3 <L0laapk3@gmail.com>
2019-02-07 22:00:01 +0100

    initial commit

---
+++ data.lua
@@ -0,0 +1,4 @@
+
+data.raw.tool["space-science-pack"].stack_size = 200
+data.raw["rocket-silo"]["rocket-silo"].rocket_result_inventory_size = 10
+--data.raw.item.satellite.rocket_launch_products = {{"space-science-pack", 200}, {"space-science-pack", 200}, {"space-science-pack", 200}, {"space-science-pack", 200}, {"space-science-pack", 200}}
